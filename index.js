6716b784374e9ba527afe2acf4d051047908ad5a
rezwan-hossen <rezwanhossen625@gmail.com>
2024-05-13 09:37:28 +0600

    mongodb add and get data

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5001;
 const app = express();
@@ -11,6 +12,40 @@ const corsOption = {
 app.use(cors(corsOption));
 app.use(express.json());
 
+//=========================================================
+
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.p5jkrsj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const serviccol = client.db("serviceDB").collection("service");
+    //get all data from db
+    app.get("/service", async (req, res) => {
+      const cursor = serviccol.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+run().catch(console.dir);
+
+//=============================================================================
 app.get("/", (req, res) => {
   res.send("hello from doccare...");
 });
